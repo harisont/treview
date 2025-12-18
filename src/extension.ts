f5b103478dc03e7e2228b0dd5084987c27f17f35
Arianna Masciolini <uzkamascio@gmail.com>
2025-12-18 17:12:51 +0100

    basic renamings

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('treeview.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('treeview.previewTree', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from treeview!');
@@ -23,4 +23,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
